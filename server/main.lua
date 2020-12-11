b5d465b60d5a7a0dd72e80468be7f50ccb1a5ed7
Ali <74619504+Utinax@users.noreply.github.com>
2020-12-11 19:07:15 +0300

    Mythic_notify'a geçildi.

--- server/main.lua
+++ server/main.lua
@@ -4,23 +4,24 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent('utx_nufus:kimlikver')
 AddEventHandler('utx_nufus:kimlikver', function(item, count)
-	local xPlayer = ESX.GetPlayerFromId(source)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
 	local money = xPlayer.getMoney()
 	if xPlayer.getInventoryItem('kimlik').count > 0 then
-		xPlayer.showNotification('Kimliğinizi zaten temin etmişsiniz!')
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'Kimliğiniz zaten temin etmişsiniz!'} )
 	elseif money > 0 then
 		xPlayer.removeMoney(Config.Ucret)
 		Citizen.Wait(250)
 		xPlayer.addInventoryItem(item, count)
-		xPlayer.showNotification('Kimliğiniz şahsınıza tahsis edildi!')
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'Kimliğiniz şahsınıza tahsis edildi!'} )
 	else
-		xPlayer.showNotification('Üzerinizde yeterli para yok!')
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'Üzerinizde yeterli para yok!'} )
 	end
 end)
 
 RegisterServerEvent('utx_nufus:isim')
 AddEventHandler('utx_nufus:isim', function(firstname)
-    local _source = source
+	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	
 	if(xPlayer.getMoney() >= Config.IsimUcret) then
@@ -30,19 +31,19 @@ AddEventHandler('utx_nufus:isim', function(firstname)
             ['@firstname'] = firstname,
             ['@identifier'] = xPlayer.identifier
         })
-        xPlayer.showNotification("Yeni isminiz: ".. firstname)
+	TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'Yeni isminiz: '.. firstname} )
         if xPlayer.getInventoryItem('kimlik').count > 0 then
 			xPlayer.removeInventoryItem('kimlik', 1)
-			xPlayer.showNotification("İsim değişikliğinden dolayı yeni bir kimlik almanız gerekecek, eskisi kaldırıldı.")
+			TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'İsim değişikliğinden dolayı yeni bir kimlik almanız gerekecek, eskisi kaldırıldı.'} )
 		end
 	else
-		xPlayer.showNotification("Yeterli paranız yok.")
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'Yeterli paranız yok.'} )
 	end
 end)
 
 RegisterServerEvent('utx_nufus:soyisim')
 AddEventHandler('utx_nufus:soyisim', function(lastname)
-    local _source = source
+	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
 	
 	if(xPlayer.getMoney() >= Config.SoyisimUcret) then
@@ -52,13 +53,13 @@ AddEventHandler('utx_nufus:soyisim', function(lastname)
 			['@lastname'] = lastname,
 			['@identifier'] = xPlayer.identifier
 		})
-		xPlayer.showNotification("Yeni soyisminiz: ".. lastname)
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'Yeni soyisminiz: '.. lastname} )
 		if xPlayer.getInventoryItem('kimlik').count > 0 then
 			xPlayer.removeInventoryItem('kimlik', 1)
-			xPlayer.showNotification("Soyisim değişikliğinden dolayı yeni bir kimlik almanız gerekecek, eskisi kaldırıldı.")
+			TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'YSoyisim değişikliğinden dolayı yeni bir kimlik almanız gerekecek, eskisi kaldırıldı.'} )
 		end
 	else
-		xPlayer.showNotification("Yeterli paranız yok.")
+		TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'Yeterli paranız yok.'} )
 	end
 end)
 
